e7d03187dce6e0b2776338d7e6ac6610a6932531
GacusPL <kapiszponar123@gmail.com>
2026-01-26 21:57:26 +0100

    Create actions.ts

---
+++ src/app/admin/actions.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import { createClient } from '@/utils/supabase/server'
+
+async function isUserAdmin() {
+  const supabase = await createClient()
+  const { data: { user } } = await supabase.auth.getUser()
+  if (!user) return false
+  const { data: profile } = await supabase.from('profiles').select('is_admin').eq('id', user.id).single()
+  return profile?.is_admin === true
+}
